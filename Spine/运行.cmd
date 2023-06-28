af4faf6deb6886b53cee3e1d59b5b005e4ac2b40
yefq <31612605+yefq@users.noreply.github.com>
2023-06-28 11:01:23 +0800

    修复导出配置

--- Spine/运行.cmd
+++ Spine/运行.cmd
@@ -15,13 +15,16 @@ if defined json (
 	set input_name=%~n1
     set source_atlas_file=%input_folder%\%input_name%.atlas.txt
 
-    set export_setting=.\setting.export.json
+    set export_setting=.\export.json
 
     set output_folder=.\_export\%input_name%\
 
     set output_project_file=%output_folder%\%input_name%.spine
     set output_images_folder=%output_folder%\images\
     set output_export_folder=%output_folder%\_export\
+
+    rem 删除导出文件夹
+    rmdir /s /q %output_folder%
 
     echo.
 
@@ -33,6 +36,8 @@ if defined json (
     rem 解包图集文件
     Spine -i %input_folder% -o %output_images_folder% -c %source_atlas_file%
 
+    echo.
+
     rem 用配置重新导出一次
     Spine -i %output_project_file% -m -o %output_export_folder% -e %export_setting%
 
@@ -41,3 +46,9 @@ if defined json (
     echo.
 )
 goto Input
+
+
+rem === Functions ===
+:ResolvePath
+    set %1=%~dpfn2
+    exit /b
